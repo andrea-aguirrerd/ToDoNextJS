a4b0f341aabd7237f1fe820d344a263b518d9a65
Andrea A. Aguirre <i-andrea.aguirre@HNDDEVINT02.local>
2024-04-22 16:21:50 -0600

    Added links to home page menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,53 @@
-import Link from 'next/link';
-import NavBar from './components/NavBar';
+import Link from "next/link";
+import NavBar from "./components/NavBar";
 
-const menuOptions = ["List all ToDos", "Pagination", , "List API elements", "Delete information", "Update Information", "Create information"];
+const menuOptions = [
+  "List all ToDos",
+  "List API elements",
+  "Create information",
+  "Update Information",
+  "Pagination",
+];
 
-function menuLinks(index:number){
-  switch(index){
-    
+function menuLinks(index: number): string {
+  switch (index) {
+    case 0:
+      return "/todos";
+    case 1:
+      return "/list-elements";
+    case 2:
+      return "create-element";
+    case 3:
+      return "update-element";
+    case 4:
+      return "/pagination";
+
+    default:
+      "/";
   }
-
-
+  return "/";
 }
 
 export default function Home() {
   return (
     <main>
-    <NavBar></NavBar>
-    <div className="flex min-h-screen flex-col items-center justify-between pt-7 bg-gradient-to-r from-slate-700 to-green-600">
-    <div className="flex h-screen justify-items-center flex-col gap-2">
-      <h1 className="justify-center text-white text-4xl font-mono">
-        {" "}
-        To Do APP
-      </h1>
-      {menuOptions.map((option, index) => (
-        <div key={index} className="rounded-md border p-1 overflow-auto grid text-center hover:bg-gray-400 text">
-         {/* <Link href={
-           
-          }>
-            {option}
-          </Link>*/}
+      <NavBar></NavBar>
+      <div className="flex min-h-screen flex-col items-center justify-between pt-7 bg-gradient-to-r from-slate-700 to-green-600">
+        <div className="flex h-screen justify-items-center flex-col gap-2">
+          <h1 className="justify-center text-white text-4xl font-mono">
+            {" "}
+            To Do APP
+          </h1>
+          {menuOptions.map((option, index) => (
+            <div
+              key={index}
+              className="rounded-md border p-1 overflow-auto grid text-center hover:bg-gray-400 text"
+            >
+              <Link href={menuLinks(index)}>{option}</Link>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
-    </div>
+      </div>
     </main>
   );
 }
---
+++ app/update-element/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+
+const UpdateElement = () => {
+  return;
+  <>
+    <NavBar />
+    <h1>hello</h1>
+  </>;
+};
+
+export default UpdateElement;
